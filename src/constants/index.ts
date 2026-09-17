507f638117d8e40ba1932352183e289a14520a81
kinga-a <56465232+kinga-a@users.noreply.github.com>
2026-09-17 23:17:04 +0800

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 // 应用常量定义
 
 export const APP_CONFIG = {
-  name: 'F个人导航',
+  name: 'F-Nav',
   version: '1.0.0',
   description: '现代化云端导航页面',
   githubUrl: 'https://github.com/eallion/favorite-cloudflare',
