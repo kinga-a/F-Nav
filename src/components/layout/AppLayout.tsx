879ffbf1e6ddf07c63986a1879b68b451221c061
kinga-a <56465232+kinga-a@users.noreply.github.com>
2026-09-25 01:03:44 +0800

    修复分类点击

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -5,6 +5,7 @@ import { useCategoriesContext } from '../../contexts/CategoriesContext';
 import { useConfigContext } from '../../contexts/ConfigContext';
 import { useSearch } from '../../hooks/useSearch';
 import { useDataSync } from '../../hooks/useDataSync';
+import { scrollToCategory } from '../../utils/scrollToCategory';
 import { toast } from '../../../components/Toast';
 import { Header } from './Header';
 import { Sidebar } from './Sidebar';
@@ -286,7 +287,7 @@ export function AppLayout() {
 
   const handleUnlockCategory = useCallback((cat: Category) => {
     if (cat.hasPassword && !unlockedCategoryIds.has(cat.id)) setCatAuthModalData(cat);
-    else document.getElementById(`cat-${cat.id}`)?.scrollIntoView();
+    else scrollToCategory(cat.id);
   }, [unlockedCategoryIds]);
 
   const handleCategoryUnlock = useCallback((id: string) => {
