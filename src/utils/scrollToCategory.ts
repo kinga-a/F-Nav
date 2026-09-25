f82710aa79b95ce2de4514b7472040f4a3d37581
kinga-a <56465232+kinga-a@users.noreply.github.com>
2026-09-26 00:21:36 +0800

    Update scrollToCategory.ts

--- src/utils/scrollToCategory.ts
+++ src/utils/scrollToCategory.ts
@@ -31,7 +31,7 @@ export function scrollToCategory(categoryId: string) {
       const h = header.getBoundingClientRect().height;
       if (h > 0) return h;
     }
-    return 80;
+    return 100;
   };
 
   const jump = () => {
